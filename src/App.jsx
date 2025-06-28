ef33d2a401cef056af316d109f78d6e6299a708a
Sujay PV <sujaylords100@gmail.com>
2025-06-28 23:03:54 +0530

    UI done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './Components/Weather'
+import './Components/Weather.css'
+
+
+const App = () => {
+  return (
+    <div><Weather /></div>
+  )
+}
+
+export default App
